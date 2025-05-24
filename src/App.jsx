9ae6ffcf174dc6efacd27821d65eb1e7ba95b677
kangwa-ntema <99867229+kangwa-ntema@users.noreply.github.com>
2025-05-24 08:38:53 +0200

    set display of outlet routes to center of screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,39 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 
-import LandingPage from './components/LandingPage';
+import LandingPage from "./components/LandingPage";
 import LoginForm from "./components/LoginForm";
-import ChangePasswordForm from './components/ChangePasswordForm';
+import ChangePasswordForm from "./components/ChangePasswordForm";
 import Dashboard from "./components/Dashboard";
-import Customers from './components/Customers';
-import AddCustomerForm from './components/AddCustomerForm';
-import Accounting from './components/Accounting';
-import './App.css'
+import Customers from "./components/Customers";
+import AddCustomerForm from "./components/AddCustomerForm";
+import Accounting from "./components/Accounting";
+import "./App.css";
 
 function App() {
-
   return (
     <BrowserRouter>
-    <div className="appPrimaryNavbarContainer">
+      <div className="appPrimaryNavbarContainer">
         <header className="appPrimaryNavbar">
-          <div  className="logo">
-            Red Diamond Solutions. 
-          </div>
+          <div className="logo">Red Diamond Solutions.</div>
         </header>
       </div>
-      <Routes>
-        <Route path="/" element={<LandingPage/>} />
-        <Route path="/loginForm" element={<LoginForm/>} />
-        <Route path="/dashboard" element={<Dashboard/>} />
-        <Route path="/customers" element={<Customers/>} />
-        <Route path="/changePasswordForm" element={<ChangePasswordForm/>}/>
-        <Route path="/customers/add" element={<AddCustomerForm/>}/>
-        <Route path="/accounting" element={<Accounting/>}/>
-      </Routes>
-       <footer className="appFooter">
+      <div className="outletSection">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/loginForm" element={<LoginForm />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/customers" element={<Customers />} />
+          <Route path="/changePasswordForm" element={<ChangePasswordForm />} />
+          <Route path="/customers/add" element={<AddCustomerForm />} />
+          <Route path="/accounting" element={<Accounting />} />
+        </Routes>
+      </div>
+      <footer className="appFooter">
         <p className="appFooterCopyright">Red Diamond Solutions &copy; 2025</p>
       </footer>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
